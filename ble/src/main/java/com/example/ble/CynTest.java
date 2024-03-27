6136b9e7ec54e7d20f75b54045e64a67b9f5d90d
15225692907 <540435039@qq.com>
2024-03-27 15:09:40 +0800

    first commit

---
+++ ble/src/main/java/com/example/ble/CynTest.java
@@ -0,0 +1,15 @@
+package com.example.ble;
+
+/**
+ * 项目名称：CeApplication
+ * 创建人： PANZERS
+ * 创建时间：2024/3/2714:53
+ * 修改人：PANZERS
+ * 修改时间：2024/3/2714:53
+ * 修改备注：
+ */
+public class CynTest {
+    public int calculation(int number){
+        return number*100;
+    }
+}
